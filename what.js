365a433820b5cabc3aa9c5eca104ab5eeca5873c
lgentile06 <egentile@umich.edu>
2017-08-11 13:20:50 -0400

    update all

---
+++ what.js
@@ -0,0 +1,12 @@
+var x = 6;
+var y = 4;
+var a = function(b) 
+{ return function(c) { return y + b + c; } 
+};
+x = 2;
+y = 5;
+var fn = a(x);
+x = 1;
+y = 3;
+var unknown = ?;
+console.log(fn(unknown));
